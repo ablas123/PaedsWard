2ff0d76aff69013870672242e518ac209043a2cf
ablas123 <67266977+ablas123@users.noreply.github.com>
2026-07-10 14:45:14 +0200

    Update EventBus.js

--- public/js/core/EventBus.js
+++ public/js/core/EventBus.js
@@ -1,31 +1,93 @@
+// CoreWard - EventBus (Pub/Sub Pattern)
+// Central event system for component communication
+
 class EventBus {
   constructor() {
-    this.events = {};
+    this.listeners = Object.create(null); // cleaner than {}
   }
 
+  /**
+   * Subscribe to an event
+   * @param {string} event - Event name
+   * @param {Function} callback - Handler function
+   * @returns {Function} Unsubscribe function
+   */
   on(event, callback) {
-    if (!this.events[event]) this.events[event] = [];
-    this.events[event].push(callback);
+    if (typeof callback !== 'function') {
+      throw new TypeError('EventBus.on: callback must be a function');
+    }
+    if (!this.listeners[event]) this.listeners[event] = [];
+    this.listeners[event].push(callback);
+
+    // Return unsubscribe function
+    return () => this.off(event, callback);
   }
 
+  /**
+   * Unsubscribe from an event
+   * @param {string} event - Event name
+   * @param {Function} callback - Handler to remove
+   */
   off(event, callback) {
-    if (!this.events[event]) return;
-    this.events[event] = this.events[event].filter(cb => cb !== callback);
+    if (!this.listeners[event]) return;
+    if (!callback) {
+      // Remove all listeners for this event
+      delete this.listeners[event];
+      return;
+    }
+    this.listeners[event] = this.listeners[event].filter(cb => cb !== callback);
+    if (this.listeners[event].length === 0) {
+      delete this.listeners[event];
+    }
   }
 
+  /**
+   * Emit an event with data
+   * @param {string} event - Event name
+   * @param {any} data - Data to pass to listeners
+   */
   emit(event, data) {
-    if (!this.events[event]) return;
-    this.events[event].forEach(cb => cb(data));
+    if (!this.listeners[event]) return;
+    // Copy array to prevent mutation during iteration
+    const handlers = this.listeners[event].slice();
+    handlers.forEach(handler => {
+      try {
+        handler(data);
+      } catch (err) {
+        console.error(`[EventBus] Error in handler for "${event}":`, err);
+      }
+    });
   }
 
+  /**
+   * Subscribe to an event once (auto-unsubscribe after first call)
+   * @param {string} event - Event name
+   * @param {Function} callback - Handler function
+   */
   once(event, callback) {
-    const wrapper = (data) => {
+    const wrapped = (data) => {
+      this.off(event, wrapped);
       callback(data);
-      this.off(event, wrapper);
     };
-    this.on(event, wrapper);
+    this.on(event, wrapped);
+  }
+
+  /**
+   * Check if event has listeners
+   * @param {string} event - Event name
+   * @returns {boolean}
+   */
+  has(event) {
+    return !!(this.listeners[event] && this.listeners[event].length > 0);
+  }
+
+  /**
+   * Clear all listeners
+   */
+  clear() {
+    this.listeners = Object.create(null);
   }
 }
 
-const bus = new EventBus();
-window.bus = bus;
+// Global singleton instance
+window.EventBus = new EventBus();
